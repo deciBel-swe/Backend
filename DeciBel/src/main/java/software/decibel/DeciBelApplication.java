6096f7e7b79cbe0c2965d7ab50932f52f040672f
Haya Elnaggar <haya.ahmed05@eng-st.cu.edu.eg>
2026-04-23 10:42:12 +0200

    fix(search):remove mistake in main

--- DeciBel/src/main/java/software/decibel/DeciBelApplication.java
+++ DeciBel/src/main/java/software/decibel/DeciBelApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsyncplease go through all the endpoints you made and check validations and authorization
-also check if it is affected by a module that wasn't implemented at the timeplease go through all the endpoints you made and check validations and authorization
-also check if it is affected by a module that wasn't implemented at the time
+@EnableAsync
 public class DeciBelApplication {
 
     public static void main(String[] args) {
